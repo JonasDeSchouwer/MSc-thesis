085c7dda2b4b402f9f313426575ea036337e010c
Jonas De Schouwer <jonasdeschouwer@gmail.com>
2024-07-18 00:12:38 +0100

    updated jobs to run on ARC

--- run/run_experiments.sh
+++ run/run_experiments.sh
@@ -16,7 +16,7 @@ function run_repeats {
         return 1
     fi
 
-    main="python main.py --cfg ${cfg_file}"
+    main="$DATA/.conda/envs/graphgps/bin/python main.py --cfg ${cfg_file}"
     out_dir="results/${dataset}"  # <-- Set the output dir.
     common_params="out_dir ${out_dir} ${cfg_overrides}"
 
@@ -24,8 +24,8 @@ function run_repeats {
     echo "  output dir: ${out_dir}"
 
     # Run each repeat as a separate job
-    for SEED in {0..9}; do
-        script="sbatch ${slurm_directive} -J ${cfg_suffix}-${dataset} run/wrapper.sb ${main} --repeat 1 seed ${SEED} ${common_params}"
+    for SEED in {0..5}; do
+        script="sbatch --job-name ${cfg_suffix}-${dataset} ${slurm_directive} ${main} --repeat 1 seed ${SEED} ${common_params}"
         echo $script
         eval $script
     done
@@ -48,73 +48,77 @@ done
 # Comment-out runs that you don't want to submit.
 cfg_dir="configs/GPS"
 
-DATASET="zinc"
-slurm_directive="--time=0-15:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
-run_repeats ${DATASET} GPS+RWSE "name_tag GPSwRWSE.10run"
-
-
 DATASET="mnist"
-slurm_directive="--time=0-5:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
-run_repeats ${DATASET} GPS "name_tag GPSwLapPE.GatedGCN+Trf.10run"
+slurm_directive="--partition=medium --mem=16G --gres=gpu:1 --cpus-per-task=4"
+
+run_repeats ${DATASET} GCN "name_tag GCNwLapPE"
+run_repeats ${DATASET} GPS-Transformer "name_tag GPSwLapPE.GINE+Transformer"
+run_repeats ${DATASET} GPS-Performer "name_tag GPSwLapPE.GINE+Performer"
+run_repeats ${DATASET} GPS-SparseAttention "name_tag GPSwLapPE.GINE+SparseMHA"
 
 
-DATASET="cifar10"
-slurm_directive="--time=0-5:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
-run_repeats ${DATASET} GPS "name_tag GPSwLapPE.GatedGCN+Trf.10run"
+# DATASET="zinc"
+# slurm_directive="--time=0-15:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
+# run_repeats ${DATASET} GPS+RWSE "name_tag GPSwRWSE.10run"
 
 
-DATASET="pattern"
-slurm_directive="--time=0-6:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
-run_repeats ${DATASET} GPS "name_tag GPSwLapPE.GatedGCN+Trf.10run  wandb.project PATTERN-fix"
+# DATASET="cifar10"
+# slurm_directive="--time=0-5:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
+# run_repeats ${DATASET} GPS "name_tag GPSwLapPE.GatedGCN+Trf.10run"
 
 
-DATASET="cluster"
-slurm_directive="--time=0-6:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
-run_repeats ${DATASET} GPS "name_tag GPSwLapPE.GatedGCN+Trf.10run  wandb.project CLUSTER-fix"
+# DATASET="pattern"
+# slurm_directive="--time=0-6:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
+# run_repeats ${DATASET} GPS "name_tag GPSwLapPE.GatedGCN+Trf.10run  wandb.project PATTERN-fix"
 
 
-DATASET="ogbg-molhiv"
-slurm_directive="--time=0-6:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
-run_repeats ${DATASET} GPS+RWSE "name_tag GPSwRWSE.GatedGCN+Trf.10run"
+# DATASET="cluster"
+# slurm_directive="--time=0-6:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
+# run_repeats ${DATASET} GPS "name_tag GPSwLapPE.GatedGCN+Trf.10run  wandb.project CLUSTER-fix"
 
 
-DATASET="ogbg-molpcba"
-slurm_directive="--time=0-12:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
-run_repeats ${DATASET} GPS+RWSE "name_tag GPSwRWSE.10run"
+# DATASET="ogbg-molhiv"
+# slurm_directive="--time=0-6:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
+# run_repeats ${DATASET} GPS+RWSE "name_tag GPSwRWSE.GatedGCN+Trf.10run"
 
 
-DATASET="ogbg-code2"
-slurm_directive="--time=1-6:00:00 --mem=24G --gres=gpu:1 --cpus-per-task=4"
-run_repeats ${DATASET} GPS "name_tag GPSnoPE.10run"
+# DATASET="ogbg-molpcba"
+# slurm_directive="--time=0-12:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
+# run_repeats ${DATASET} GPS+RWSE "name_tag GPSwRWSE.10run"
 
 
-DATASET="ogbg-ppa"  # NOTE: for ogbg-ppa we need SBATCH --mem=48G
-run_repeats ${DATASET} GPS "name_tag GPSnoPE.GatedGCN+Perf.lyr3.dim256.drp01.wd-5.10run"
+# DATASET="ogbg-code2"
+# slurm_directive="--time=1-6:00:00 --mem=24G --gres=gpu:1 --cpus-per-task=4"
+# run_repeats ${DATASET} GPS "name_tag GPSnoPE.10run"
 
 
-DATASET="pcqm4m"  # NOTE: for PCQM4Mv2 we need SBATCH --mem=48G and up to 3days runtime; run only one repeat
-slurm_directive="--time=3-00:00:00 --mem=48G --gres=gpu:1 --cpus-per-task=4"
-#run_repeats ${DATASET} GPS+RWSE "name_tag GPSwRWSE.small.lyr5.dim304  train.ckpt_best True"
-#run_repeats ${DATASET} GPSmedium+RWSE "name_tag GPSwRWSE.medium.lyr10.dim384.heads16.drp01.attndrp01.lr0002.e150  train.ckpt_best True"
-
-run_repeats ${DATASET} GPSmedium+RWSE "name_tag GPSwRWSE.medium train.ckpt_best True"
-run_repeats ${DATASET} GPSmedium+RWSE "name_tag GPSwRWSE.medium.gelu.linlr  optim.scheduler linear_with_warmup gnn.act gelu train.ckpt_best True"
-
-slurm_directive="--time=4-00:00:00 --mem=48G --gres=gpu:1 --cpus-per-task=4"
-run_repeats ${DATASET} GPSlarge+RWSE "name_tag GPSwRWSE.large train.ckpt_best True"
+# DATASET="ogbg-ppa"  # NOTE: for ogbg-ppa we need SBATCH --mem=48G
+# run_repeats ${DATASET} GPS "name_tag GPSnoPE.GatedGCN+Perf.lyr3.dim256.drp01.wd-5.10run"
 
 
-DATASET="malnettiny"
-run_repeats ${DATASET} GPS-noPE  "name_tag GPS-noPE.GatedGCN+Perf.lyr5.dim64.10runs"
-run_repeats ${DATASET} GPS-noPE  "name_tag GPS-noPE.GatedGCN+Trf.lyr5.dim64.bs4.bacc4.10run  train.batch_size 4 optim.batch_accumulation 4 gt.layer_type CustomGatedGCN+Transformer"
+# DATASET="pcqm4m"  # NOTE: for PCQM4Mv2 we need SBATCH --mem=48G and up to 3days runtime; run only one repeat
+# slurm_directive="--time=3-00:00:00 --mem=48G --gres=gpu:1 --cpus-per-task=4"
+# #run_repeats ${DATASET} GPS+RWSE "name_tag GPSwRWSE.small.lyr5.dim304  train.ckpt_best True"
+# #run_repeats ${DATASET} GPSmedium+RWSE "name_tag GPSwRWSE.medium.lyr10.dim384.heads16.drp01.attndrp01.lr0002.e150  train.ckpt_best True"
+
+# run_repeats ${DATASET} GPSmedium+RWSE "name_tag GPSwRWSE.medium train.ckpt_best True"
+# run_repeats ${DATASET} GPSmedium+RWSE "name_tag GPSwRWSE.medium.gelu.linlr  optim.scheduler linear_with_warmup gnn.act gelu train.ckpt_best True"
+
+# slurm_directive="--time=4-00:00:00 --mem=48G --gres=gpu:1 --cpus-per-task=4"
+# run_repeats ${DATASET} GPSlarge+RWSE "name_tag GPSwRWSE.large train.ckpt_best True"
+
+
+# DATASET="malnettiny"
+# run_repeats ${DATASET} GPS-noPE  "name_tag GPS-noPE.GatedGCN+Perf.lyr5.dim64.10runs"
+# run_repeats ${DATASET} GPS-noPE  "name_tag GPS-noPE.GatedGCN+Trf.lyr5.dim64.bs4.bacc4.10run  train.batch_size 4 optim.batch_accumulation 4 gt.layer_type CustomGatedGCN+Transformer"
 
 
 
 ################################################################################
 ##### SAN
 ################################################################################
-cfg_dir="configs/SAN"
-slurm_directive="--time=1-00:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
+# cfg_dir="configs/SAN"
+# slurm_directive="--time=1-00:00:00 --mem=16G --gres=gpu:1 --cpus-per-task=4"
 
 #DATASET="pattern"
 #run_repeats ${DATASET} SAN "name_tag SAN.10run-fix  wandb.project PATTERN-fix"
