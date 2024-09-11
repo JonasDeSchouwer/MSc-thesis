49f08216b937cacdee1f7046cd79affd8b2f0679
Jonas De Schouwer <jonasdeschouwer@gmail.com>
2024-09-11 10:41:19 +0200

    fixed missing import

--- run/report_epoch_times.py
+++ run/report_epoch_times.py
@@ -1,5 +1,11 @@
-from graphgps.utils import report_epoch_times2
+import os
+import sys
 import argparse
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from graphgps.utils import report_epoch_times2
 
 parser = argparse.ArgumentParser(description='Report epoch times')
 parser.add_argument('dir', type=str, help='Directory containing the runs')
