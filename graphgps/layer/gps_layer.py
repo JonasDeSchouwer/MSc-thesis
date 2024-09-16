fixed nasty bug in ModelNet loading code; enabled adaptive use of gradient checkpointing

graphgps/layer/gps_layer.py
@@ -7,11 +7,14 @@ from performer_pytorch import SelfAttention
 from torch_geometric.data import Batch
 from torch_geometric.nn import Linear as Linear_pyg
 from torch_geometric.utils import to_dense_batch
+from torch.utils.checkpoint import checkpoint
 
 from graphgps.layer.gatedgcn_layer import GatedGCNLayer
 from graphgps.layer.gine_conv_layer import GINEConvESLapPE
 from graphgps.layer.bigbird_layer import SingleBigBirdLayer
 from graphgps.layer.sparse_attention_layer import SparseAttention
+
+from torch_geometric.graphgym.config import cfg
 
 
 class GPSLayer(nn.Module):
@@ -141,6 +144,26 @@ class GPSLayer(nn.Module):
         self.ff_dropout1 = nn.Dropout(dropout)
         self.ff_dropout2 = nn.Dropout(dropout)
 
+    def _apply_self_attn(self, h, graph_ids):
+        """Apply self-attention to the graph nodes.
+        """
+
+        # h_dense: [num_graphs, N_max, F_node]
+        # mask: [num_graphs, N_max] -> each entry is True if it is a 'real' node, and False if it is a 'padding' node
+        h_dense, mask = to_dense_batch(h, graph_ids)
+        if self.global_model_type == 'Transformer':
+            h_attn = self._sa_block(h_dense, None, ~mask)[mask]
+        elif self.global_model_type == 'Performer':
+            h_attn = self.self_attn(h_dense, mask=mask)[mask]
+        elif self.global_model_type == 'BigBird':
+            h_attn = self.self_attn(h_dense, attention_mask=mask)
+        elif self.global_model_type == 'SparseAttention':
+            h_attn = self.self_attn(h_dense)[mask]
+        else:
+            raise RuntimeError(f"Unexpected {self.global_model_type}")
+
+        return h_attn
+
     def forward(self, batch):
         # batch.num_graphs
         # batch.batch: [N_total] -> each entry is which graph that node belongs to
@@ -184,19 +207,13 @@ class GPSLayer(nn.Module):
 
         # Multi-head attention.
         if self.self_attn is not None:
-            # h_dense: [num_graphs, N_max, F_node]
-            # mask: [num_graphs, N_max] -> each entry is True if it is a 'real' node, and False if it is a 'padding' node
-            h_dense, mask = to_dense_batch(h, batch.batch)
-            if self.global_model_type == 'Transformer':
-                h_attn = self._sa_block(h_dense, None, ~mask)[mask]
-            elif self.global_model_type == 'Performer':
-                h_attn = self.self_attn(h_dense, mask=mask)[mask]
-            elif self.global_model_type == 'BigBird':
-                h_attn = self.self_attn(h_dense, attention_mask=mask)
-            elif self.global_model_type == 'SparseAttention':
-                h_attn = self.self_attn(h_dense)[mask]
+            
+            if cfg.share.gradient_checkpointing:
+                h_attn = checkpoint(GPSLayer._apply_self_attn,
+                    self, h, batch.batch
+                )
             else:
-                raise RuntimeError(f"Unexpected {self.global_model_type}")
+                h_attn = self._apply_self_attn(h, batch.batch)
 
             h_attn = self.dropout_attn(h_attn)
             h_attn = h_in1 + h_attn  # Residual connection.

graphgps/loader/dataset/modelnet_on_disk.py
@@ -137,9 +137,15 @@ class ModelNetOnDisk(Dataset):
             for target, category in enumerate(categories):
                 folder = osp.join(self.raw_dir, category, split)
                 if split == 'train':
-                    train_paths.extend(glob.glob(f'{folder}/{category}_*.off'))
+                    train_paths.extend([
+                        (path, target) for path
+                        in glob.glob(f'{folder}/{category}_*.off')
+                    ])
                 else:
-                    test_paths.extend(glob.glob(f'{folder}/{category}_*.off'))
+                    test_paths.extend([
+                        (path, target) for path
+                        in glob.glob(f'{folder}/{category}_*.off')
+                    ])
 
         # shuffle the train and test paths
         random.shuffle(train_paths)
@@ -147,7 +153,7 @@ class ModelNetOnDisk(Dataset):
 
         # save the chunks of the train set to disk
         current_train_chunk_id = 0
-        for path in train_paths:
+        for path, target in train_paths:
             data: Data = read_off(path)
             data.y = torch.tensor([target])
             train_graphs_buffer.append(data)
@@ -172,7 +178,7 @@ class ModelNetOnDisk(Dataset):
 
         # save the chunks of the test set to disk
         current_test_chunk_id = 0
-        for path in test_paths:
+        for path, target in test_paths:
             data: Data = read_off(path)
             data.y = torch.tensor([target])
             test_graphs_buffer.append(data)

graphgps/train/custom_train.py
@@ -27,7 +27,7 @@ def arxiv_cross_entropy(pred, true, split_idx):
     return loss, pred_score
 
 
-def _is_OOM_error(e: RuntimeError, batch):
+def _is_OOM_error(e: RuntimeError):
     """
     Args:
         e: RuntimeError
@@ -75,7 +75,8 @@ def train_epoch(logger, loader, model, optimizer, scheduler, batch_accumulation)
         # we implement a system to make performance degradation due to OOM errors more graceful
         # for each batch:
         # try 0: process the entire batch on the GPU
-        # try 1: split the batch into its components and process them one by one on the GPU. Skip the ones that don't work
+        # try 1: split the batch into its components and process them one by one on the GPU.
+        # try 2: for each component, if it doesn't work normally then try with gradient checkpointing. If that doesn't work, skip the component.
         # (never done: CPU processing, as it is too slow)
 
         batch_copy = batch.clone()
@@ -86,7 +87,7 @@ def train_epoch(logger, loader, model, optimizer, scheduler, batch_accumulation)
             _true, _pred, loss = _get_single_batch_gradients(model, batch, loader, batch_accumulation)
             get_gradients_succeeded = True
         except RuntimeError as e:
-            if _is_OOM_error(e, batch):
+            if _is_OOM_error(e):
                 logging.info(f"OOM error occurred on batch {batch}. Retrying after splitting batch.")
         
         # --- TRY 1 ---
@@ -107,8 +108,21 @@ def train_epoch(logger, loader, model, optimizer, scheduler, batch_accumulation)
                     _preds.append(_pred_batch)
                     losses.append(loss_batch)
                 except RuntimeError as e:
-                    if _is_OOM_error(e, batch):
-                        continue    # skip this element of the batch
+                    if _is_OOM_error(e):
+                        logging.info(f"OOM error occurred on element of batch. Retrying with gradient checkpointing.")
+                        # try again with gradient checkpointing
+                    try:
+                        cfg.share.gradient_checkpointing = True
+                        _true_batch, _pred_batch, loss_batch = _get_single_batch_gradients(model, Batch.from_data_list([data]), loader, batch_accumulation)
+                        _trues.append(_true_batch)
+                        _preds.append(_pred_batch)
+                        losses.append(loss_batch)
+                    except RuntimeError as e:
+                        if _is_OOM_error(e):
+                            pass    # skip this element of the batch
+                    finally:
+                        cfg.share.gradient_checkpointing = False
+                        
             
             # if all elements of the batch failed, skip the batch
             if len(_trues) == 0:
@@ -117,6 +131,7 @@ def train_epoch(logger, loader, model, optimizer, scheduler, batch_accumulation)
             else:
                 # still report how many elements of the batch failed
                 logging.info(f"OOM error occurred on {len(data_list) - len(_trues)} elements of batch.")
+                get_gradients_succeeded = True
 
             # otherwise, aggregate the _trues, _preds, and losses for logging purposes
             _true = torch.cat(_trues, dim=0)