f84882d0e3ece8ab57dc40eb3a4ade55f23ff3f8
Jonas De Schouwer <jonasdeschouwer@gmail.com>
2024-10-01 02:09:29 +0200

    enabled loading on OnDiskDatasets without my hack within imported modules

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ from torch_geometric import seed_everything
 from graphgps.finetuning import load_pretrained_model_cfg, \
     init_model_from_pretrained
 from graphgps.logger import create_logger
+from graphgps.utils import my_create_loader
 
 
 def new_optimizer_config(cfg):
@@ -69,7 +70,6 @@ def custom_set_run_dir(cfg, run_id):
         run_id (int): Main for-loop iter id (the random seed or dataset split)
     """
     cfg.run_dir = os.path.join(cfg.out_dir, str(run_id))
-    logging.info(f"[*] Output directory for this run: {cfg.run_dir}")
     # Make output directory
     if cfg.train.auto_resume:
         os.makedirs(cfg.run_dir, exist_ok=True)
@@ -138,7 +138,7 @@ if __name__ == '__main__':
                      f"split_index={cfg.dataset.split_index}")
         logging.info(f"    Starting now: {datetime.datetime.now()}")
         # Set machine learning pipeline
-        loaders = create_loader()
+        loaders = my_create_loader()
         loggers = create_logger()
         # custom_train expects three loggers for 'train', 'valid' and 'test'.
         # GraphGym code creates one logger/loader for each of the 'train_mask' etc.
